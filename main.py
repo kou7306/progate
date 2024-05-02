342bc7260d1fd26e28be320d4c23519e340b3143
Am <ayumu.univ@gmail.com>
2024-05-02 21:51:29 +0900

    narrow_down中身完成

--- main.py
+++ main.py
@@ -15,14 +15,64 @@ supabase: Client = create_client(url, key)
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+#ベタ打ち（削除予定）
+main_place=(139.405457,35.694031)#仮置き
+lon,lat=main_place
+n=0.1
+print(main_place)
+    # テーブル名と条件を指定
+table_name = 'address'
+
+    # Supabaseのデータベースからデータを取得
+response = supabase.table(table_name).select('id', 'longitude', 'latitude').execute()
+print(response,type(response))
+#data,count = response
+data=response.data
+print(data)
+    # 条件を満たすIDを取得
+result = [row['id'] for row in data if ((row['longitude'] - lon)**2 + (row['latitude'] - lat)**2 <= n)]
+
+lis=[]
+for row in data:
+    lis.append((((row['longitude'] - lon)**2 + (row['latitude'] - lat)**2),row['id']))
+
+print()
+print(result)
+print("(距離,id)",lis)
+#ここまで
+
 
 @app.get("/")
 async def read_root(request: Request):
     response = supabase.table('address').select("*").execute()
     context = {"request": request, "data": response.data}
-    print(response.data)
+    #print(response.data)
     return templates.TemplateResponse("index.html", context)
 
 @app.post("/")
-def narrow_down():
-    return
+async def narrow_down():
+    main_place=(139.405457,35.694031)#仮置き
+    lon,lat=main_place
+    n=0.1
+    print(main_place)
+        # テーブル名と条件を指定
+    table_name = 'address'
+
+        # Supabaseのデータベースからデータを取得
+    response = supabase.table(table_name).select('id', 'longitude', 'latitude').execute()
+    print(response,type(response))
+    #data,count = response
+    data=response.data
+    print(data)
+        # 条件を満たすIDを取得
+    result = [row['id'] for row in data if ((row['longitude'] - lon)**2 + (row['latitude'] - lat)**2 <= n)]
+
+    lis=[]
+    for row in data:
+        lis.append((((row['longitude'] - lon)**2 + (row['latitude'] - lat)**2),row['id']))
+
+    print()
+    print(result)
+    print("(距離,id)",lis)
+
+    return result
